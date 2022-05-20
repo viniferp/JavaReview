08ca1681a8d49815a4cd14b2a4f4bf5a72d74234
augusto.pinto <augustfp18@gmail.com>
2022-05-20 18:08:56 -0300

    Generics Review

---
+++ src/application/Main.java
@@ -0,0 +1,55 @@
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+/***
+ * Generics Review ex1
+ * 		
+ */
+//		List <Shape> figures = new ArrayList<Shape>();
+//		figures.add(new Rectangle(2.5, 2.5));
+//		figures.add(new Circle(3.2));
+//		System.out.println(GenericsReview.figuresListArea(figures));
+		
+/***
+ * Generics Review ex2
+ */
+//		List<Integer> listInteger = new ArrayList<Integer>();
+//		listInteger.add(1);
+//		listInteger.add(2);
+//		listInteger.add(3);
+//		
+//		List<Double> listDouble = new ArrayList<Double>();
+//		listDouble.add(1.5);
+//		listDouble.add(2.5);
+//		listDouble.add(3.5);
+//		
+//		List<Object> destiny = new ArrayList<Object>();
+//		GenericsReview.copy(listInteger, destiny);
+//		GenericsReview.copy(listDouble, destiny);
+//		
+//		for (Object object : destiny) {
+//			System.out.println(object);
+//		}
+		
+	}
+}
